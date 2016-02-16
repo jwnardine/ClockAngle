77e4ddd86420f907af3facad2fe55c81f16b9a4b
Jon Nardine and Ryan Brown <jwnardine@gmail.com>
2016-02-15 16:06:29 -0800

    Add passing tests for each angle circumstance

--- src/Clock.php
+++ src/Clock.php
@@ -2,9 +2,19 @@
 
 class Clock
 {
-    function checkAngle($hour, $minute)
-    {
-
+    function checkAngle($hour, $minute) {
+        $hourOutput = ((30 * $hour) + ($minute / 2));
+        $minuteOutput = ($minute * 6);
+        $hourMinuteDiff = ($hourOutput - $minuteOutput);
+            if ($hourMinuteDiff > 180) {
+                return (360 - $hourMinuteDiff);
+            } elseif (($hourMinuteDiff < 0) && ($hourMinuteDiff > -180)) {
+                return abs($hourMinuteDiff);
+            } elseif ($hourMinuteDiff < -180) {
+                return ($hourMinuteDiff + 360);
+            } else {
+                return $hourMinuteDiff;
+        }
     }
 }
 
--- tests/ClockTest.php
+++ tests/ClockTest.php
@@ -18,6 +18,45 @@ class ClockTest extends PHPUnit_Framework_TestCase
         //Assert
         $this->assertEquals(180, $result);
     }
+    function test_checkAngle_BiggerThanOneEighty()
+    {
+        //Arrange
+        $test_Clock = new Clock;
+        $input1 = 9;
+        $input2 = 00;
+
+        //Act
+        $result = $test_Clock->checkAngle($input1, $input2);
+
+        //Assert
+        $this->assertEquals(90, $result);
+    }
+    function test_checkAngle_BiggerThanNegativeOneEighty()
+    {
+        //Arrange
+        $test_Clock = new Clock;
+        $input1 = 5;
+        $input2 = 57;
+
+        //Act
+        $result = $test_Clock->checkAngle($input1, $input2);
+
+        //Assert
+        $this->assertEquals(163.5, $result);
+    }
+    function test_checkAngle_lessThanNegativeOneEighty()
+    {
+        //Arrange
+        $test_Clock = new Clock;
+        $input1 = 1;
+        $input2 = 57;
+
+        //Act
+        $result = $test_Clock->checkAngle($input1, $input2);
+
+        //Assert
+        $this->assertEquals(76.5, $result);
+    }
 
 }
 
